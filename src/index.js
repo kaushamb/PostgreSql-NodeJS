ad02529b835a23d643680bf9b823698698ba3c32
kaushamb tiwari <kaushambtiwari@gmail.com>
2026-07-24 22:31:00 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT
+
+//middleware 
+app.use(express.json());
+app.use(cors)
+
+//error handling
+
+//server running 
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port} `)
+})
