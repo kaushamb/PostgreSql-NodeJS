db66b31e40f4eeb0e3d8b617aac483a63d75e3aa
kaushamb tiwari <kaushambtiwari@gmail.com>
2026-07-29 02:55:40 +0530

    Model added for create user update user delete user get user

---
+++ src/models/userModel.js
@@ -0,0 +1,22 @@
+import pool from "../config/db.js"
+
+export const createUser=(id)=>{
+   const result = await pool.query("INSERT INTO users (name, email) VALUES($1,$2) RETURNING *",[name, email]);
+    return result.rows[0];
+}
+export const getAllUsers=()=>{
+   const result = await pool.query("SELECT * FROM users");
+   return result.rows;
+}
+export const getUserById=(id)=>{
+   const result = await pool.query("SELECT * FROM users WHERE id=$1",[id]);
+   return result.rows[0];
+}
+export const updateUser=(id, name, email)=>{
+    const result = await pool.query("UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *",[name, email, id]);
+    return result.rows[0];
+}
+export const deleteUser=(id)=>{
+    const result = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *"[id]);
+    return result.rows[0];
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@ import express from "express";
 const router= express.Router();
 
 router.get("/user",getAllUsers)
-router.get("/user/1:id",getUserByIdUsers)
+router.get("/user/:id",getUserById)
 router.post("/user/:id",createUser)
 router.put("/user/:id",updateUser)
 router.delete("/user/:id",deleteUser)
